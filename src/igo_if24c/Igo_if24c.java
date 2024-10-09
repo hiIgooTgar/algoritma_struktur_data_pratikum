5c513aad23db082d6641cca9b4f1f4e7986470ef
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-09 23:51:17 +0700

    Algoritma dan Struktur Data | Pratikum 2

---
+++ src/igo_if24c/Igo_if24c.java
@@ -0,0 +1,42 @@
+package igo_if24c;
+
+import java.util.Scanner;
+
+public class Igo_if24c {
+    
+
+    public static void main(String[] args) {
+    
+        Scanner coba = new Scanner(System.in);
+        
+        System.out.print("Masukkan NIM anda: ");
+        String nim = coba.nextLine();
+        System.out.print("Masukkan Nama anda: ");
+        String nama = coba.nextLine();
+        System.out.println("Pilih program studi : ");
+        System.out.println("1. Informatika");
+        System.out.println("2. Sistem Informasi"); 
+        System.out.println("3. Teknologi Informasi");
+        System.out.print("Masukkan pilihan anda (1/2/3) : ");
+        int pilihan = coba.nextInt();
+        
+        String prodi;
+        
+        switch(pilihan) {
+            case 1:
+                prodi = "Informatika";
+                break;
+            case 2:
+                prodi = "Sistem Informasi";
+                break;
+            case 3:
+                prodi = "Teknologi Informasi";
+                break;            default:
+                prodi = "Tidak memilih program studi";
+         }
+        
+        System.out.println(nama + " dengan NIM " + nim + " memilih program studi " + prodi);
+        
+    }
+    
+}
