c543f8b0597a9be2b5f6c64299ad8abd50630e16
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-10 20:03:35 +0700

    Sistem Basis Data | Pratikum 1 - Introducation

---
+++ src/Pratikum_1/Practice_first_pratikum.java
@@ -0,0 +1,20 @@
+package Pratikum_1;
+
+public class Practice_first_pratikum {
+
+    public static void main(String[] args) {
+        System.out.println("======= Data Mahasiswa =======");
+        System.out.println("");
+        System.out.println("NIM : 24SA11A159");
+        System.out.println("Nama : Igo Tegar Prambudhy");
+        System.out.println("Prodi : Informatika");
+        System.out.println("Fakultas : Ilmu Komputer");
+        System.out.println("Alamat : Purbalingga");
+        System.out.println("");
+        System.out.println("=== Selesai | Terima Kasih ===");
+        System.out.println("");
+        System.out.println("======= Pratikum Ke-1 ========");
+        System.out.println("========= Hello Java =========");
+    }
+    
+}
