18e580ce098cd0a47390c029cb40b1ae60e55443
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-19 23:34:38 +0700

    Algoritma dan Struktur Data | Pratikum Update

---
+++ src/igo_pratikum_1/latihan_java.java
@@ -0,0 +1,41 @@
+package igo_pratikum_1;
+
+public class latihan_java {
+
+    public static void main(String[] args) {
+        
+        System.out.println("===== Data Mahasiswa =====");
+        System.out.println("Nama : Igo Tegar Prambudhy");
+        System.out.println("NIM : 24SA11A159");
+        System.out.println("Program Studi : Informtika");
+        System.out.println("Fakultas : Ilmu Komputer");
+        System.out.println("");
+        System.out.println("==========================");
+        System.out.println("== Bahasa Pemrograman : ==");
+        System.out.println("==========================");
+        System.out.println("Java");
+        System.out.println("C");
+        System.out.println("C++");
+        System.out.println("C#");
+        System.out.println("Python");
+        System.out.println("Javascript");
+        System.out.println("PHP");
+        System.out.println("CSS");
+        System.out.println("HTML");
+        System.out.println("Ruby");
+        System.out.println("Golang");
+        System.out.println("");
+        System.out.println("=========================");
+        System.out.println("======== Software =======");
+        System.out.println("=========================");
+        System.out.println("Visual Studio Code");
+        System.out.println("Netbeans 8.2");
+        System.out.println("DEV C++");
+        System.out.println("Postman");
+        System.out.println("Sublime Text");
+        System.out.println("Notepad++");
+        System.out.println("XAMPP");
+        System.out.println("SQLyog");
+    }
+    
+}
