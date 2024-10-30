d80b754b8a6afc521684ac287ba92eca8f8787ac
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-30 17:01:37 +0700

    ASD | Pratikum 5 - Update

--- src/igo_pratikum_5/if_else.java
+++ src/igo_pratikum_5/if_else.java
@@ -1,35 +1,39 @@
 package igo_pratikum_5;
+//import Scanner
 import java.util.Scanner;
-
 
 public class if_else {
 
     public static void main(String[] args) {
+        //mendeklarasikan scanner
         Scanner input = new Scanner(System.in);
         
+        // mendeklarasikan variabel
         int nilai;
         String grade;
         
+        //cetak perintah nilai dengan type data integer
         System.out.print("Masukkan Nilai anda : ");
         nilai = input.nextInt();
         
-        if(nilai >= 90) {
+       
+        if(nilai >= 90) { // Kita cek, jika nilai lebih dari sama dengan 90 maka tampilkan grade "A"
             grade = "A";
-        } else if(nilai >= 80) {
+        } else if(nilai >= 80) { // dan jika nilai lebih dari sama dengan 80 maka tampilkan grade "B+"
             grade = "B+";
-        } else if(nilai >= 70) {
+        } else if(nilai >= 70) { // dan jika nilai lebih dari sama dengan 70 maka tampilkan grade "B"
             grade = "B";
-        } else if(nilai >= 60) {
+        } else if(nilai >= 60) { // dan jika nilai lebih dari sama dengan 60 maka tampilkan grade "C+"
             grade = "C+";
-        } else if(nilai >= 50) {
+        } else if(nilai >= 50) { // dan jika nilai lebih dari sama dengan 50 maka tampilkan grade "C"
             grade = "C";
-        } else if(nilai >= 40) {
+        } else if(nilai >= 40) { // dan jika nilai lebih dari sama dengan 40 maka tampilkan grade "D"
             grade = "D";
-        } else {
+        } else { // dan jika nilai kurang dari 40 maka tampilkan grade "E"
             grade = "E";
         }
         
-        // cetak hasil
+        // cetak hasil grade dengan memanggil variabel grade
         System.out.println("Grade anda : " + grade);
     }
     
