d66aaf0c2de1d38b3d795b1e36e476ec142231a6
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-12-06 18:39:47 +0700

    Algortima dan Struktur Data | Praktikum 9 - Update

--- src/igo_pratikum_9/pratikum_9_tugas_no_1.java
+++ src/igo_pratikum_9/pratikum_9_tugas_no_1.java
@@ -5,7 +5,7 @@ public class pratikum_9_tugas_no_1 {
     public static void main(String[] args) {
            /* mendeklarasikan array dengan tipe data string dan nama variabel array 
         nya adalah siswa dengan panjang array 3 dan melakukan inisialisasi nilai awal */
-         String[] siswa = {"Ahmad", "Badrun", "Chyntia"};
+         String[] siswa = {"Ahmad", "Samsul", "Chyntia"};
          siswa[1] = "Badrun"; // Mengubah isi array siswa yaitu index ke 1 dengan "Badrun"
          
          // Melakukan perulangan For Each yaitu membuat tipe data beserta elemenya
--- src/igo_pratikum_9/pratikum_9_tugas_no_2.java
+++ src/igo_pratikum_9/pratikum_9_tugas_no_2.java
@@ -6,12 +6,12 @@ public class pratikum_9_tugas_no_2 {
     public static void main(String[] args) {
         // mendeklarasikan Scanner
         Scanner input = new Scanner(System.in);
+        // mendeklarasikan variabel dan memberikan inisialisasi nilai awal
+        int value = 6;
          /* mendeklarasikan array dengan tipe data string dan
          nama variabel array nya adalah siswa dengan panjang array 6.
         dibawah contoh pembuatan array dengan menggabungkan deklarasi dengan instansiasi */
-        String[] siswa = new String[6];
-        // mendeklarasikan variabel dan memberikan inisialisasi nilai awal
-        int value = 6;
+        String[] siswa = new String[value];
         
         /* Misal nilai awalnya i = 0; kondisionalnya adalah i kurang dari variabel value yaitu 6;
         dan diiterasikan bertambahkan 1 / ++ increment*/
