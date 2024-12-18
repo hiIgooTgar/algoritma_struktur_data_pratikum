fd98f05d3cefa7564386abecad381ad5ebb48b3a
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-12-18 19:33:40 +0700

    Algortima dan Struktur Data | Praktikum 11 - Sorting - Insertion Sort

--- src/igo_praktikum_11/insertion_sort.java
+++ src/igo_praktikum_11/insertion_sort.java
@@ -1,10 +1,9 @@
 package igo_praktikum_11;
-
 public class insertion_sort {
-
+    /* Membuat sebuah method dengan nama insertion_sort dengan mempunyai parameter yaitu arr dengan tipe data integer serta bersifat array */
     public static void insertion_sort(int[] arr) {
-        int n = arr.length;
-        
+        int n = arr.length;  // melakukan inisialisasi variabel
+         // syntax untuk melakukan sorting insertion sort
         for(int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
@@ -16,9 +15,10 @@ public class insertion_sort {
             arr[j + 1] = key;
         }
     }
-    
+    /* Membuat sebuah method dengan nama printArray dengan mempunyai parameter yaitu arr dengan tipe data integer serta bersifat array */
     public static void printArry(int[] arr) {
-        int n = arr.length;
+        int n = arr.length; // melakukan inisialisasi variabel
+        // syntax untuk menampilkan hasil array
         for(int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
@@ -26,15 +26,11 @@ public class insertion_sort {
     }
     
     public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6};
-        
+        int[] arr = {12, 11, 13, 5, 6}; /* mendeklarasikan variabel, yaitu varibel arr yang bersifat array dengan tipe data integer */
         System.out.println("Array sebelum diurutkan : ");
-        printArry(arr);
-        
-        insertion_sort(arr);
-        
+        printArry(arr); // memanggil method printArray dengan mempunyai parameter 
+        insertion_sort(arr); // memanggil method insertion_sort dengan mempunyai parameter 
         System.out.println("Array setelah diurutkan : ");
-        printArry(arr);
+        printArry(arr); // memanggil method printArray dengan mempunyai parameter 
     }
-    
 }
