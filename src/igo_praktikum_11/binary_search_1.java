4a4a5d9ca908b5d0a377c228a00c462284be1dea
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-12-18 18:57:20 +0700

    Algortima dan Struktur Data | Praktikum 11 - Searching - Binary Search

--- src/igo_praktikum_11/binary_search_1.java
+++ src/igo_praktikum_11/binary_search_1.java
@@ -1,32 +1,34 @@
 package igo_praktikum_11;
-
 public class binary_search_1 {
 
     public static void main(String[] args) {
+         /* mendeklarasikan variabel, yaitu varibel nilai yang bersifat array dengan tipe data integer */
         int[] nilai = {66, 77, 80, 84, 88, 99, 100};
-        binarySearchInt(nilai, 77);
+        binarySearchInt(nilai, 77);  // memanggil method binarySearchInt dengan mempunyai parameter
     }
-    
+    /* Membuat sebuah method dengan nama binarySearchInt dengan mempunyai parameter yaitu angka dengan tipe data integer serta
+    bersifat array dan key dengan tipe data integer */
     public static void binarySearchInt(int[] angka, int key) {
-        int low = 0, high = angka.length-1, median;
+        int low = 0, high = angka.length-1, median; // mendeklarasikan variabel dan melakukan inisialisasi nilai variabel
         boolean status = false;
-        while(low <= high) {
-            median = (low + high)/2;
-     
+        while(low <= high) { /* Misal nilai awalnya low = 0; kondisionalnya adalah low kurang dari sama dengan high */
+            median = (low + high)/2; // melakukan inisialiasi variabel
+            // cek, jika variabel key sama dengan angka[median]) maka
             if(key == angka[median]) {
-                System.out.println(key + " ditemukan pada posisi ke-" + (median+1) + " dan pada indeks ke-" + median);
-                status = true;
-                break;
+                // tampilkan perintah (key + " ditemukan pada posisi ke-" + (median+1) + " dan pada indeks ke-" + median)
+                System.out.println(key + " ditemukan pada posisi ke-" + (median+1) + " dan pada indeks ke-" + median); 
+                status = true; // status menjadi true
+                break; // dan lakukan break
             } else {
+                 // cek, jika variabel key lebih dari angka[median]) maka
                 if(key > angka[median]) {
-                    low = median + 1;
+                    low = median + 1; // lakukan inisialisasi low = median + 1
                 } else {
-                    high = median - 1;
+                    high = median - 1; // jika tidak // lakukan inisialisasi high = median - 1
                 }
             }
         }
-        
-        if(status == false) {
+        if(status == false) { // cek, jika variabel status sama dengan false/0 maka tampilkan (key + " tidak ditemukan dalam array")
             System.out.println(key + " tidak ditemukan dalam array");
         }
     } 
